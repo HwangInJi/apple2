ffde33b73e4b0da045a1cd782ec7b6bc25bcd3ed
HwangInJi <dlswl1993@gmail.com>
2024-07-05 15:24:00 +0900

    저장

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import Highlight from "./components/Highlight";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Highlight />
+      <Model />
+    </>
+  );
+};
+
+export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,47 @@
+import apple from "/assets/images/apple.svg";
+import search from "/assets/images/search.svg";
+import bag from "/assets/images/bag.svg";
+
+import hmv from "/assets/videos/hero.mp4";
+import smallmv from "/assets/videos/smallHero.mp4";
+
+import watch from "/assets/images/watch.svg";
+import right from "/assets/images/right.svg";
+
+import highlightFirstmv from "/assets/videos/highlight-first.mp4";
+import highlightSecondmv from "/assets/videos/hightlight-sec.mp4";
+import highlightThirdmv from "/assets/videos/hightlight-third.mp4";
+import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
+
+import pause from "/assets/images/pause.svg";
+import play from "/assets/images/play.svg";
+import replay from "/assets/images/replay.svg";
+
+import yellow from "/assets/images/yellow.jpg";
+import blue from "/assets/images/blue.jpg";
+import white from "/assets/images/white.jpg";
+import black from "/assets/images/black.jpg";
+
+export const appleImg = apple;
+export const searchImg = search;
+export const bagImg = bag;
+
+export const heroVideo = hmv;
+export const smallHeroVideo = smallmv;
+
+export const watchImg = watch;
+export const rightImg = right;
+
+export const highlightFirstVideo = highlightFirstmv;
+export const highlightSecondVideo = highlightSecondmv;
+export const highlightThirdVideo = highlightThirdmv;
+export const highlightFourthVideo = highlightFourthmv;
+
+export const pauseImg = pause;
+export const playImg = play;
+export const replayImg = replay;
+
+export const yellowImg = yellow;
+export const blueImg = blue;
+export const whiteImg = white;
+export const blackImg = black;
